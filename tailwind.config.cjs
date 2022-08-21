10b87684ebb64d22988d0480c50d43d486a9d6e2
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-08-22 03:30:08 +0700

    feat(tailwind): added default tailwind colors and another

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      black: colors.black,
+      white: colors.white,
+      slate: colors.slate,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      neutral: colors.neutral,
+      stone: colors.stone,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      sky: colors.sky,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      fuchsia: colors.fuchsia,
+      pink: colors.pink,
+      rose: colors.rose,
+      main: '#2c2c38',
+      secondary: '#21212d',
+      button: '#726dd2',
+    },
     extend: {},
   },
   plugins: [],
