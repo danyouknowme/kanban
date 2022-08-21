c65275ddbdccee77338a7c830788218e5edd3e61
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-08-22 05:53:09 +0700

    feat(sidebar): create sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Navbar } from "./components";
+import { Navbar, Sidebar } from "./components";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<Navbar />
-			<div className="bg-secondary min-h-screen text-white">
-				hello Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus in rerum repellat incidunt facere distinctio recusandae iste
-				blanditiis libero ducimus commodi delectus praesentium, tempore ratione culpa similique obcaecati ipsa? Nesciunt optio, natus, quasi impedit
-				nostrum consequatur voluptates fuga ratione sunt labore laboriosam maiores eaque vel rerum suscipit. Quod, corrupti libero?
-			</div>
+		<div className="App min-h-screen bg-whitesmoke dark:bg-secondary">
+			<Sidebar>
+				<div className="flex flex-col">
+					<Navbar />
+					<div className="text-white p-6">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem earum voluptas laborum accusantium voluptates, eaque facere non porro quidem
+						sapiente ipsam quia reprehenderit dolor? Eum qui consequatur aspernatur velit dignissimos culpa quasi veritatis harum expedita maxime,
+						voluptas facere animi repellendus, quae quaerat dicta, similique sequi vel et. Adipisci, illum vel?
+					</div>
+				</div>
+			</Sidebar>
 		</div>
 	);
 };
