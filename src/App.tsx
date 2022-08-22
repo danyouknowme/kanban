4ec767374eddd957aa2007bb580ef9b2ee88010d
danyouknowme <thanathip.suw@gmail.com>
2022-08-23 01:48:11 +0700

    feat(task): create task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { Navbar, Sidebar } from "./components";
+import { Navbar, Sidebar, Task } from "./components";
+import { mockData } from "../mock";
 
 const App: React.FC = () => {
-	return (
-		<div className="App min-h-screen bg-whitesmoke dark:bg-secondary">
-			<Sidebar>
-				<div className="flex flex-col">
-					<Navbar />
-					<div className="text-white p-6">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem earum voluptas laborum accusantium voluptates, eaque facere non porro quidem
-						sapiente ipsam quia reprehenderit dolor? Eum qui consequatur aspernatur velit dignissimos culpa quasi veritatis harum expedita maxime,
-						voluptas facere animi repellendus, quae quaerat dicta, similique sequi vel et. Adipisci, illum vel?
-					</div>
-				</div>
-			</Sidebar>
-		</div>
-	);
+  return (
+    <div className="App min-h-screen bg-whitesmoke dark:bg-secondary overflow-hidden">
+      <Sidebar>
+        <div className="flex flex-col flex-1">
+          <Navbar />
+          <div className="flex gap-x-6 p-6 overflow-x-auto w-screen md:w-[calc(100vw-304px)] h-[calc(100vh-96px)]">
+            {mockData.map((data, index) => (
+              <Task task={data} key={index} />
+            ))}
+          </div>
+        </div>
+      </Sidebar>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Task.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ITask } from "../interfaces/task";
+import TaskList from "./TaskList";
+
+interface TaskProps {
+  task: ITask;
+}
+
+const Task: React.FC<TaskProps> = ({ task }) => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center">
+        <div className="w-3 h-3 rounded-full" style={{ backgroundColor: task.color }}></div>
+        <span className="ml-3 text-zinc-500 text-xs tracking-widest uppercase">{task.taskName} (4)</span>
+      </div>
+      <div className="flex flex-col">
+        {task.taskList.map((tasklist, index) => (
+          <TaskList tasklist={tasklist} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Task;
