50d276528cc4e6907e3c823130ed57bcad26fefd
danyouknowme <thanathip.suw@gmail.com>
2022-08-23 15:36:50 +0700

    fixed(react): downgrade react version

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { ThemeProvider } from "./contexts/theme";
 import { Provider } from "react-redux";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider initialTheme={"dark"}>
         <App />
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
