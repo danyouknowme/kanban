40bb94f147e88c9b1632eedd620cb22095bea65c
danyouknowme <thanathip.suw@gmail.com>
2022-08-23 16:28:23 +0700

    fixed(theme): remove unused code and rename file

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,26 +1,26 @@
 import { BsFillSunFill, BsFillMoonStarsFill } from "react-icons/bs";
-import { useThemeContext } from "../hooks/theme";
+import { useThemeContext } from "../hooks/useThemeContext";
 
 const ThemeToggle = ({ isOpenSideBar }: { isOpenSideBar: boolean }) => {
-	const { theme, setTheme } = useThemeContext();
+  const { theme, setTheme } = useThemeContext();
 
-	return (
-		<div
-			className={`flex justify-center items-center w-64 py-3 rounded mb-6 duration-300 ${
-				isOpenSideBar ? "bg-button dark:bg-secondary" : "bg-white dark:bg-main"
-			}`}
-			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-		>
-			<BsFillSunFill className="w-4 h-4 text-zinc-400" />
-			<div className="relative w-12 h-5 rounded-xl mx-6 bg-white dark:bg-button">
-				<div
-					className="absolute top-1/2 -translate-y-1/2 w-4 h-4 rounded-full duration-500 
+  return (
+    <div
+      className={`flex justify-center items-center w-64 py-3 rounded mb-6 duration-300 ${
+        isOpenSideBar ? "bg-button dark:bg-secondary" : "bg-white dark:bg-main"
+      }`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <BsFillSunFill className="w-4 h-4 text-zinc-400" />
+      <div className="relative w-12 h-5 rounded-xl mx-6 bg-white dark:bg-button">
+        <div
+          className="absolute top-1/2 -translate-y-1/2 w-4 h-4 rounded-full duration-500 
 					left-1 bg-button dark:left-7 dark:bg-white"
-				></div>
-			</div>
-			<BsFillMoonStarsFill className="w-4 h-4 text-zinc-400" />
-		</div>
-	);
+        ></div>
+      </div>
+      <BsFillMoonStarsFill className="w-4 h-4 text-zinc-400" />
+    </div>
+  );
 };
 
 export default ThemeToggle;
--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -1,48 +1,48 @@
-import { useState, useEffect, createContext, ReactNode, Dispatch, SetStateAction, useContext } from "react";
+import { useState, useEffect, createContext, ReactNode, Dispatch, SetStateAction } from "react";
 
 const getInitialTheme = () => {
-	if (typeof window !== "undefined" && window.localStorage) {
-		const storedPrefs = window.localStorage.getItem("color-theme");
-		if (typeof storedPrefs === "string") {
-			return storedPrefs;
-		}
+  if (typeof window !== "undefined" && window.localStorage) {
+    const storedPrefs = window.localStorage.getItem("color-theme");
+    if (typeof storedPrefs === "string") {
+      return storedPrefs;
+    }
 
-		const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
-		if (userMedia.matches) {
-			return "dark";
-		}
-	}
+    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userMedia.matches) {
+      return "dark";
+    }
+  }
 
-	return "light";
+  return "light";
 };
 
 interface ThemeContextValue {
-	theme: string;
-	setTheme: Dispatch<SetStateAction<string>>;
+  theme: string;
+  setTheme: Dispatch<SetStateAction<string>>;
 }
 
 export const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
 
 export const ThemeProvider = ({ initialTheme, children }: { initialTheme: string; children: ReactNode }) => {
-	const [theme, setTheme] = useState<string>(getInitialTheme);
+  const [theme, setTheme] = useState<string>(getInitialTheme);
 
-	const rawSetTheme = (rawTheme: string) => {
-		const root = window.document.documentElement;
-		const isDark = rawTheme === "dark";
+  const rawSetTheme = (rawTheme: string) => {
+    const root = window.document.documentElement;
+    const isDark = rawTheme === "dark";
 
-		root.classList.remove(isDark ? "light" : "dark");
-		root.classList.add(rawTheme);
+    root.classList.remove(isDark ? "light" : "dark");
+    root.classList.add(rawTheme);
 
-		localStorage.setItem("color-theme", rawTheme);
-	};
+    localStorage.setItem("color-theme", rawTheme);
+  };
 
-	if (initialTheme) {
-		rawSetTheme(initialTheme);
-	}
+  if (initialTheme) {
+    rawSetTheme(initialTheme);
+  }
 
-	useEffect(() => {
-		rawSetTheme(theme);
-	}, [theme]);
+  useEffect(() => {
+    rawSetTheme(theme);
+  }, [theme]);
 
-	return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
 };
