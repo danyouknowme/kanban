7321903fd90817160e7969ad1420a41ddad936f3
danyouknowme <thanathip.suw@gmail.com>
2022-08-23 11:20:55 +0700

    fixed(mock): edit mockup data

--- mock.ts
+++ mock.ts
@@ -1,88 +1,93 @@
 export const mockData = [
   {
-    taskName: "todo",
-    taskList: [
+    boardName: "Platform Launch",
+    boardTask: [
       {
-        name: "Build UI onboarding flow",
-        subtasks: [],
+        taskName: "todo",
+        taskList: [
+          {
+            name: "Build UI onboarding flow",
+            subtasks: [],
+          },
+          {
+            name: "Build UI for search",
+            subtasks: [],
+          },
+          {
+            name: "Build settings UI",
+            subtasks: [],
+          },
+          {
+            name: "QA and test all major user journeys",
+            subtasks: [],
+          },
+        ],
+        color: "#22d3ee",
       },
       {
-        name: "Build UI for search",
-        subtasks: [],
+        taskName: "doing",
+        taskList: [
+          {
+            name: "Design settings and search pages",
+            subtasks: [],
+          },
+          {
+            name: "Add account management endpoints",
+            subtasks: [],
+          },
+          {
+            name: "Design onboarding flow",
+            subtasks: [],
+          },
+          {
+            name: "Add search endpoints",
+            subtasks: [],
+          },
+          {
+            name: "Add authentication endpoints",
+            subtasks: [],
+          },
+          {
+            name: "Research pricing points of various competitors and trail different bussiness models",
+            subtasks: [],
+          },
+        ],
+        color: "#726dd2",
       },
       {
-        name: "Build settings UI",
-        subtasks: [],
-      },
-      {
-        name: "QA and test all major user journeys",
-        subtasks: [],
+        taskName: "done",
+        taskList: [
+          {
+            name: "Conduct 5 wireframe tests",
+            subtasks: [],
+          },
+          {
+            name: "Create wireframe prototype",
+            subtasks: [],
+          },
+          {
+            name: "Review results of usability tests and iterate",
+            subtasks: [],
+          },
+          {
+            name: "Create paper prototypes and conduct 10 usability tests with potential customers",
+            subtasks: [],
+          },
+          {
+            name: "Market discovery",
+            subtasks: [],
+          },
+          {
+            name: "Competitor analysis",
+            subtasks: [],
+          },
+          {
+            name: "Research the market",
+            subtasks: [],
+          },
+        ],
+        color: "#86efad",
       },
     ],
-    color: "#22d3ee",
-  },
-  {
-    taskName: "doing",
-    taskList: [
-      {
-        name: "Design settings and search pages",
-        subtasks: [],
-      },
-      {
-        name: "Add account management endpoints",
-        subtasks: [],
-      },
-      {
-        name: "Design onboarding flow",
-        subtasks: [],
-      },
-      {
-        name: "Add search endpoints",
-        subtasks: [],
-      },
-      {
-        name: "Add authentication endpoints",
-        subtasks: [],
-      },
-      {
-        name: "Research pricing points of various competitors and trail different bussiness models",
-        subtasks: [],
-      },
-    ],
-    color: "#726dd2",
-  },
-  {
-    taskName: "done",
-    taskList: [
-      {
-        name: "Conduct 5 wireframe tests",
-        subtasks: [],
-      },
-      {
-        name: "Create wireframe prototype",
-        subtasks: [],
-      },
-      {
-        name: "Review results of usability tests and iterate",
-        subtasks: [],
-      },
-      {
-        name: "Create paper prototypes and conduct 10 usability tests with potential customers",
-        subtasks: [],
-      },
-      {
-        name: "Market discovery",
-        subtasks: [],
-      },
-      {
-        name: "Competitor analysis",
-        subtasks: [],
-      },
-      {
-        name: "Research the market",
-        subtasks: [],
-      },
-    ],
-    color: "#86efad",
   },
 ];
